f8550d83b8c49957738d6c3984471dac9b4860f3
anubhavsha45 <anubhavsharma0719@gmail.com>
2026-03-11 15:04:25 +0530

    Another minor bug fix

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const appError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const bookingController = require('./controllers/bookingController');
 const app = express();
-app.enable('trust proxy');
+app.set('trust proxy', 1);
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
